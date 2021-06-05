dcdf2c16893b541e3f4e150e0ea5f08971684272
Tyler Lawson <tylaw93@yahoo.com>
2021-06-05 07:47:33 -0500

    Color theme
    Overwriting bootstrap colors with inline css

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-for(var i=8;i<13;i++){
-    let newDiv ="<div class='row'><div class='col-1 border-top border-bottom border-dark'>" + i + "</div><div class='col-10 bg-primary'>sure</div><div class='col-1 bg-success'></div></div>"
+for(var i=9;i<13;i++){
+    let newDiv ="<div class='row'><div class='col-1 border-top border-bottom border-dark'>" + i + "</div><div class='col-10' style='background-color: #c9c9c9'></div><div class='col-1 bg-success'></div></div>"
     $(".scheduler").append(newDiv)
 }
-for(var i=1;i<16;i++){
-    let newDiv ="<div class='row'><div class='col-1 border-top border-bottom border-dark'>" + i + "</div><div class='col-10 bg-primary'>sure</div><div class='col-1 bg-success'></div></div>"
+for(var i=1;i<6;i++){
+    let newDiv ="<div class='row'><div class='col-1 border-top border-bottom border-dark'>" + i + "</div><div class='col-10' style='background-color: #c9c9c9'></div><div class='col-1 bg-success'></div></div>"
     $(".scheduler").append(newDiv)
 }
-console.log("yea")
