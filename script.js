d49183a936c0407e5ede095773313837a03deafc
Tyler Lawson <tylaw93@yahoo.com>
2021-06-05 08:16:01 -0500

    Update script.js
    Made the background color of the schedule dependent on the hour

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-for(var i=9;i<13;i++){
-    let newDiv ="<div class='row'><div class='col-1 border-top border-bottom border-dark'>" + i + "</div><div class='col-10' style='background-color: #c9c9c9'></div><div class='col-1 bg-success'></div></div>"
+for(var i=9;i<18;i++){
+    if(i <= 12){
+    let newDiv ="<div class='row'><div class='col-1 border-top border-bottom border-dark'>" + i + "</div><div class='col-10 timeblock'></div><div class='col-1 bg-success'></div></div>"
+    $(".scheduler").append(newDiv)}
+    else{
+        let newDiv ="<div class='row'><div class='col-1 border-top border-bottom border-dark'>" + (i -12) + "</div><div class='col-10 timeblock'></div><div class='col-1 bg-success'></div></div>"
     $(".scheduler").append(newDiv)
+    }
 }
-for(var i=1;i<6;i++){
-    let newDiv ="<div class='row'><div class='col-1 border-top border-bottom border-dark'>" + i + "</div><div class='col-10' style='background-color: #c9c9c9'></div><div class='col-1 bg-success'></div></div>"
-    $(".scheduler").append(newDiv)
-}
+
 let today = moment().format("dddd MMMM Do")
 $("#currentDay").text(today);
+
+let currentHour = moment().format("k")
+
+
+if (currentHour < i){
+    $(".timeblock").css("background-color", "#40eb34")
+}
+else if(currentHour == i){
+    $(".timeblock").css("background-color", "#40eb34")
+}
+else{
+    $(".timeblock").css("background-color", " #c9c9c9")
+}
+
