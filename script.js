1fe2504db90df0e442ae79333cb5e4fa6b9082de
Tyler Lawson <tylaw93@yahoo.com>
2021-06-05 08:00:04 -0500

    Set up  Date
    Used moment to get and display todays date

--- script.js
+++ script.js
@@ -6,3 +6,5 @@ for(var i=1;i<6;i++){
     let newDiv ="<div class='row'><div class='col-1 border-top border-bottom border-dark'>" + i + "</div><div class='col-10' style='background-color: #c9c9c9'></div><div class='col-1 bg-success'></div></div>"
     $(".scheduler").append(newDiv)
 }
+let today = moment().format("dddd MMMM Do")
+$("#currentDay").text(today);
