73346b6be2d2b40c07fc0af2069daa13963d4303
Tyler Lawson <tylaw93@yahoo.com>
2021-06-05 07:36:41 -0500

    Jquery the divs
    Set up jquery to create all my div timeblocks

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("yes")
+for(var i=8;i<13;i++){
+    let newDiv ="<div class='row'><div class='col-1 border-top border-bottom border-dark'>" + i + "</div><div class='col-10 bg-primary'>sure</div><div class='col-1 bg-success'></div></div>"
+    $(".scheduler").append(newDiv)
+}
+for(var i=1;i<16;i++){
+    let newDiv ="<div class='row'><div class='col-1 border-top border-bottom border-dark'>" + i + "</div><div class='col-10 bg-primary'>sure</div><div class='col-1 bg-success'></div></div>"
+    $(".scheduler").append(newDiv)
+}
+console.log("yea")
