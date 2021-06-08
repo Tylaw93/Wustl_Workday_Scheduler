311bfe3f021a8efce36d08e7559d59f00fd4f306
Tyler Lawson <tylaw93@yahoo.com>
2021-06-07 23:47:48 -0500

    Refactor
    Cleaning up code and adding local storage

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-let Am9 = document.querySelector("fieldInput9")
-const Am10 = document.querySelector("fieldInput10")
-const Am11 = document.querySelector("fieldInput11")
-const Pm12 = document.querySelector("fieldInput12")
-const Pm1 = document.querySelector("timeblock1")
-const Pm2 = document.querySelector("timeblock2")
-const Pm3 = document.querySelector("timeblock3")
-const Pm4 = document.querySelector("timeblock4")
-const Pm5 = document.querySelector("timeblock5")
-
+let list  = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
+list.forEach(element => {
+    localStorage.getItem(element)
+});
 for(var i=9;i<18;i++){
     if(i <= 12){
     let newDiv ="<div class='row'><div class='col-1 border-top border-bottom border-dark text-center'>" + (i)+"AM" + "</div><div class='col-10 timeblock"+i+"' ><textarea class='form-control  border-0 timeblock"+i+" 'id = fieldInput"+i+"' rows='2'></textarea></div><div class='col-1 saveBtn'><img src='saveIcon.png' class='center'></div></div>"
@@ -22,20 +16,27 @@ for(var i=9;i<18;i++){
         $(".timeblock"+i+"").addClass("future");
         $(".timeblock"+i+"").removeClass("present");
         $(".timeblock"+i+"").removeClass("past");
-
     }
     else if(currentHour == i){
         $(".timeblock"+i+"").addClass("present");
+        $(".timeblock"+i+"").removeClass("future");
+        $(".timeblock"+i+"").removeClass("past");
     }
     else{
-        $(".timeblock"+i+"").addClass("past") ;
+        $(".timeblock"+i+"").addClass("past");
+        $(".timeblock"+i+"").removeClass("present");
+        $(".timeblock"+i+"").removeClass("future");
     }
 }
 $(document).ready(function(){
     $('.saveBtn').on('click', function(){
-        var value= $(this).siblings('.col-10').val();
-        console.log(value) 
+        event.preventDefault();
+        var value= $(this).siblings(".col-10").children().val();
+        var key = $(this).siblings(".col-1").text();
+        localStorage.setItem(key,value)
+        console.log(localStorage)
     })
+    
 })
   
 
