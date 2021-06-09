502c366c31b48de3cfc884215eaf03ad74d597ed
Tyler Lawson <tylaw93@yahoo.com>
2021-06-08 22:25:01 -0500

    Traversing Dom
    found the right element to write the lcoal storage to.

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-let list  = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
-list.forEach(element => {
-    localStorage.getItem(element)
-});
+let list  = ["9AM","10AM","11AM","12AM","1PM","2PM","3PM","4PM","5PM"];
+
 for(var i=9;i<18;i++){
     if(i <= 12){
-    let newDiv ="<div class='row'><div class='col-1 border-top border-bottom border-dark text-center'>" + (i)+"AM" + "</div><div class='col-10 timeblock"+i+"' ><textarea class='form-control  border-0 timeblock"+i+" 'id = fieldInput"+i+"' rows='2'></textarea></div><div class='col-1 saveBtn'><img src='saveIcon.png' class='center'></div></div>"
+    let newDiv ="<div class='row'><span class='col-1 border-top border-bottom border-dark text-center'>" + (i)+"AM" + "</span><div class='col-10 timeblock"+i+"' ><textarea class='form-control  border-0 timeblock"+i+" rows='2'></textarea></div><div class='col-1 saveBtn'><img src='saveIcon.png' class='center'></div></div>"
     $(".scheduler").append(newDiv)
     }
     else{
-        let newDiv ="<div class='row'><div class='col-1 border-top border-bottom border-dark text-center'>" + (i -12)+"PM" + "</div><div class='col-10 timeblock"+i+"'><textarea class='form-control  border-0 timeblock"+i+"' rows='2'></textarea></div><div class='col-1 saveBtn'><img src='saveIcon.png' class='center'></div></div>"
+        let newDiv ="<div class='row'><span class='col-1 border-top border-bottom border-dark text-center'>" + (i -12)+"PM" + "</span><div class='col-10 timeblock"+i+"'><textarea class='form-control  border-0 timeblock"+i+"' rows='2'></textarea></div><div class='col-1 saveBtn'><img src='saveIcon.png' class='center'></div></div>"
     $(".scheduler").append(newDiv)
     }
     let currentHour = moment().format("k")
@@ -28,18 +26,26 @@ for(var i=9;i<18;i++){
         $(".timeblock"+i+"").removeClass("future");
     }
 }
+
+list.forEach(element => {
+    var x = localStorage.getItem(element)
+    $("span").each(function() {
+        if(element === $(this).text()){
+            $(this).siblings(".col-10").children().text(x)
+            console.log( $(this).siblings(".col-10").children())
+        } 
+    })
+});
+
 $(document).ready(function(){
     $('.saveBtn').on('click', function(){
-        event.preventDefault();
         var value= $(this).siblings(".col-10").children().val();
         var key = $(this).siblings(".col-1").text();
         localStorage.setItem(key,value)
         console.log(localStorage)
-    })
-    
+    }) 
 })
   
-
 let today = moment().format("dddd MMMM Do")
 $("#currentDay").text(today);
 
